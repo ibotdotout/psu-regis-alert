1c30a9e4507d73f0df24585428144b3b064d0594
ibotdotout <tkroputa@gmail.com>
2014-07-28 10:03:25 +0700

    fix route to support GET/POST request

--- route.py
+++ route.py
@@ -5,12 +5,25 @@ import db_connection
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
     db = db_connection.DbConnection()
-    subject_code = flask.request.form['code']
-    email = flask.request.form['email']
-    return db.insert(subject_code, email)
+    if flask.request.method == 'POST':
+        subject_code = flask.request.form['subject_code']
+        email = flask.request.form['email']
+    else:
+        subject_code = flask.request.args.get('subject_code')
+        email = flask.request.args.get('email')
+    db.insert_item(subject_code, email)
+    return "Done"
+
+
+@app.route('/query')
+def query():
+    db = db_connection.DbConnection()
+    items = db.query_all()
+    html = ["%s, %s" % (i['subject_code'], i['email']) for i in items]
+    return '\n'.join(html)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
