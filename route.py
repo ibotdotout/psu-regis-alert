bba00c595543b0481b3363f0c4c36ab1b67885b0
ibotdotout <tkroputa@gmail.com>
2014-07-28 16:19:27 +0700

    update route to regex subject_code via url

--- route.py
+++ route.py
@@ -1,21 +1,35 @@
 import os
 import flask
 import db_connection
+import re
 
 app = flask.Flask(__name__)
+subject_code_pattern = r"(?<=subject=)\d*(?=$)"
+re_subject_code = re.compile(subject_code_pattern)
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
 
 
 @app.route('/insert', methods=['GET', 'POST'])
 def insert():
     db = db_connection.DbConnection()
     if flask.request.method == 'POST':
-        subject_code = flask.request.form['subject_code']
+        url = flask.request.form['url']
         email = flask.request.form['email']
     else:
-        subject_code = flask.request.args.get('subject_code')
+        url = flask.request.args.get('url')
         email = flask.request.args.get('email')
-    db.insert_item(subject_code, email)
-    return "Done"
+    regex_result = re_subject_code.search(url)
+    if regex_result:
+        subject_code = regex_result.group()
+        return "%s %s" % (subject_code, email)
+        db.insert_item(subject_code, email)
+        return "Done"
+    else:
+        return "Failed: Url Wrong"
 
 
 @app.route('/query')
@@ -23,15 +37,11 @@ def query():
     db = db_connection.DbConnection()
     items = db.query_all()
     if items:
-        html = ["%s, %s" % (i['subject_code'], i['email']) for i in items]
+        html = ["%s" % (i['subject_code']) for i in items]
         return '\n'.join(html)
     else:
         return "Query Failed!!!"
 
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
