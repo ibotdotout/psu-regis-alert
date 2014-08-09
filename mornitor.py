e7e2f2f35f0ae644ca358235a68ffff46cd1613b
ibotdotout <tkroputa@gmail.com>
2014-08-09 14:40:56 +0700

    add cache query subject_code

--- mornitor.py
+++ mornitor.py
@@ -7,6 +7,7 @@ import datetime
 
 db = db_connection.DbConnection()
 items = db.query_queue_all()
+queried = {}
 if items:
     print "#" * 79
     for item in items:
@@ -14,11 +15,25 @@ if items:
         print "%s %s %s" % (datetime.datetime.utcnow(), subject_code,
                             email),
         regis_alert = alert.PsuRegisAlert()
-        if regis_alert.alert(subject_code):
-            print " done"
-            subject = "[psuAlert] แจ้งเตือนลงวิชา %s" % regis_alert.subject_id
-            regis_alert._noticeEMail(email, subject, regis_alert.message)
-            db.remove(item)
+        if queried[subject_code]:
+            regis_dict = queried[subject_code]
+            if regis_dict.get('has_room', False):
+                print " done"
+                regis_alert._noticeEMail(email, regis_dict['subject'],
+                                         regis_dict['message'])
+                db.remove(item)
+            else:
+                print ""
         else:
-            print ""
+            if regis_alert.alert(subject_code):
+                print " done"
+                subject = \
+                    "[psuAlert] แจ้งเตือนลงวิชา %s" % regis_alert.subject_id
+                regis_alert._noticeEMail(email, subject, regis_alert.message)
+                db.remove(item)
+                queried[subject_code] = {'has_room': True, 'subject': subject,
+                                         'message': regis_alert.message}
+            else:
+                queried[subject_code] = {'has_room': False}
+                print ""
     print "#" * 79
