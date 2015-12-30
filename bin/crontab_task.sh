a745fb220afc020932a8af9522307e1f1bed16a2
ibotdotout <tkroputa@gmail.com>
2015-12-30 15:26:17 +0700

    Fix corntab without arguments

--- bin/crontab_task.sh
+++ bin/crontab_task.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+# Replace space with newline
+args="${1// /$'\n'}"
+
 # Fix env with space
 # http://unix.stackexchange.com/a/196761
-
 IFS='
 '
 
 #env - `cat /root/env.sh` $1 >> /var/log/cron.log 2>&1
-env - `cat /root/env.sh` $1
+env - `cat /root/env.sh` $args
