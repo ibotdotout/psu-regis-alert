9b45d5260f3175fb48934c2139d3dee4138b63a4
ibotdotout <tkroputa@gmail.com>
2018-11-06 13:17:51 +0800

    Update readme on fabric 1.x

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
+# http://www.fabfile.org/installing-1.x.html
+
 # debian
 # sudo apt-get install fabric
 
 # osx
-# pip install fabric
+# pip install 'fabric<2.0'
 
 # usage
 # run with "fab -H <host> <func>"
@@ -12,10 +14,10 @@ from fabric.api import run, local, env
 
 def backup_db():
     run('mkdir -p /root/backup')
-    run("""docker exec -d psuregisalert_db_1 sh -c '"""
+    run("""docker exec -d psu-regis-alert_db_1 sh -c '"""
         """mkdir -p backup/db_$(date +%Y-%m-%d_%Hh%Mm) """
         """ && mongodump -d psuRegisAlert """
         """ -o backup/db_$(date +%Y-%m-%d_%Hh%Mm)'""")
-    run('docker cp psuregisalert_db_1:/backup/ /root')
+    run('docker cp psu-regis-alert_db_1:/backup/ /root')
     local("rsync -r -e 'ssh -i {0}' {1}:/root/backup/ backup/"
           .format(env.key_filename[0], env.host_string))
